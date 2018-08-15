fee621cc82caba116e3e72ae13040f56fcabdc73
hiren shah <hirenshah7390@hotmail.com>
2018-08-15 00:17:36 -0500

    cleaning

--- Server/tasks/src/main/java/com/example/tasks/model/TaskStatus.java
+++ Server/tasks/src/main/java/com/example/tasks/model/TaskStatus.java
@@ -6,15 +6,6 @@ public enum TaskStatus {
     IN_PROGRESS,
     FINISHED;
 
-    /**
-     * lookup function will find the related enum element by input task status string (converted to
-     * uppercase) and return with the enum when the input string equals the name of enum element.
-     * Otherwise, return null.
-     *
-     * @param inputTaskStatusString input TaskStatus string is passed from client (frontend or other
-     *     backend) as String
-     * @return TaskStatus enum when exist and null when doesn't exist
-     */
     public static TaskStatus lookup(String inputTaskStatusString) {
         for (TaskStatus status : values()) {
             if (status.name().equals(inputTaskStatusString.toUpperCase())) {
