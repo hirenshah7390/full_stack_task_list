57186409871187e082adec4f91d0d0e0797d3aea
hiren shah <hirenshah7390@hotmail.com>
2018-08-15 00:19:25 -0500

    cleaning

--- Server/tasks/src/main/java/com/example/tasks/model/TaskStatus.java
+++ Server/tasks/src/main/java/com/example/tasks/model/TaskStatus.java
@@ -1,17 +1,7 @@
 package com.example.tasks.model;
 
 public enum TaskStatus {
-
     PENDING,
     IN_PROGRESS,
     FINISHED;
-
-    public static TaskStatus lookup(String inputTaskStatusString) {
-        for (TaskStatus status : values()) {
-            if (status.name().equals(inputTaskStatusString.toUpperCase())) {
-                return status;
-            }
-        }
-        return null;
-    }
 }
